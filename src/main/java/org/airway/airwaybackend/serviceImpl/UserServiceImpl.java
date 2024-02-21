7584b2cb53f8da60442d259c774efde954ff0d94
ofektom <okpohot@gmail.com>
2024-02-21 16:23:50 +0100

    adds setup security feature

--- src/main/java/org/airway/airwaybackend/serviceImpl/UserServiceImpl.java
+++ src/main/java/org/airway/airwaybackend/serviceImpl/UserServiceImpl.java
@@ -1,6 +1,15 @@
 package org.airway.airwaybackend.serviceImpl;
 
 import org.airway.airwaybackend.service.UserService;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
-public class UserServiceImpl implements UserService {
+@Service
+public class UserServiceImpl implements UserService, UserDetailsService {
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
 }
