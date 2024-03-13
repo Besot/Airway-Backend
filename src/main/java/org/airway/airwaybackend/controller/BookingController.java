9e85a349724f58358711c14726a719913213348a
Adedini James Oloruntoba <145149678+ADEDINIJAMES@users.noreply.github.com>
2024-03-13 21:01:12 +0100

    Flight booking Endpoint added

--- src/main/java/org/airway/airwaybackend/controller/BookingController.java
+++ src/main/java/org/airway/airwaybackend/controller/BookingController.java
@@ -1,26 +1,35 @@
 package org.airway.airwaybackend.controller;
 
 import org.airway.airwaybackend.dto.BookingRequestDto;
-import org.airway.airwaybackend.serviceImpl.BookingServiceImpl;
+import org.airway.airwaybackend.model.Booking;
+import org.airway.airwaybackend.service.BookingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/bookings")
+@RequestMapping("/api/v1/booking")
 public class BookingController {
-    private final BookingServiceImpl bookingServiceImp;
 
-    public BookingController(BookingServiceImpl bookingServiceImp) {
-        this.bookingServiceImp = bookingServiceImp;
-    }
+        private BookingService bookingService;
+        @Autowired
+        public BookingController(BookingService bookingService) {
+            this.bookingService = bookingService;
+        }
+        @GetMapping("/bookings")
+        public ResponseEntity<Page<Booking>> getAllBookings(
+                @RequestParam(defaultValue = "0") int pageNo,
+                @RequestParam(defaultValue = "10") int pageSize) {
+            return new ResponseEntity<>(bookingService.getAllBookings(pageNo, pageSize), HttpStatus.OK);
+        }
+
+
 
     @PostMapping("/booking-flight")
     public ResponseEntity<String> BookFlight(@RequestBody BookingRequestDto bookingRequestDto) {
-        String response = bookingServiceImp.bookFlight(bookingRequestDto);
+        String response = bookingService.bookFlight(bookingRequestDto);
         return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 }
--- src/main/java/org/airway/airwaybackend/service/BookingService.java
+++ src/main/java/org/airway/airwaybackend/service/BookingService.java
@@ -1,5 +1,13 @@
 package org.airway.airwaybackend.service;
 
+import org.airway.airwaybackend.dto.BookingRequestDto;
+import org.airway.airwaybackend.model.BookingFlight;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Set;
+import java.util.function.Function;
+
 import org.airway.airwaybackend.model.Booking;
 import org.springframework.data.domain.Page;
 
@@ -12,6 +20,14 @@ import java.util.Set;
 import java.util.function.Function;
 
 public interface BookingService {
+    String bookFlight(BookingRequestDto bookingRequestDto);
+    BigDecimal calculateFare(BigDecimal fare, int passenger);
+    BigDecimal getALLtotalFare (List<BookingFlight> flights);
+    String generateBookingReferenceNumber (Set<String> usedNumber);
+    String generateMemberShip (String prefix);
+    String calculateBaggageAllowance(String weightString, double factor);
+    String calculateAllBaggageAllowances(List<BookingFlight> flights, Function<BookingFlight, String> propertyExtractor);
+    BigDecimal calculateTotal(List<BookingFlight> flights, Function<BookingFlight, BigDecimal> propertyExtractor);
     Page<Booking> getAllBookings(int pageNo, int pageSize);
     int getTotalNumberOfBookings();
     String bookFlight(BookingRequestDto bookingRequestDto);
